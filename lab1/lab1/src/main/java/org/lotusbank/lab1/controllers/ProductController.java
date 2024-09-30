fd3b95c9ac4e0d9e17d46f0b5b60fff0a4bb0d95
Shawal Siddique (Shaon) <shaony10@gmail.com>
2024-09-30 14:07:14 -0500

    Practice some Lombok annotation

--- lab1/lab1/src/main/java/org/lotusbank/lab1/controllers/ProductController.java
+++ lab1/lab1/src/main/java/org/lotusbank/lab1/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package org.lotusbank.lab1.controllers;
 
+import org.lotusbank.lab1.models.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,11 @@ public class ProductController {
     @GetMapping("/")
     public String welcome(){
         return "Welcome to WAA Lab 1";
+    }
+
+    @GetMapping("/product")
+    public Product GetProduct()
+    {
+        return new Product(101,"IPhone 16","Experience the technology",1200.00);
     }
 }
---
+++ lab1/lab1/src/main/java/org/lotusbank/lab1/models/Product.java
@@ -0,0 +1,16 @@
+package org.lotusbank.lab1.models;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Data
+public class Product {
+    //@Setter(AccessLevel.NONE)
+    private Integer Id;
+    private String Name;
+    private String Description;
+    private Double Price;
+}
